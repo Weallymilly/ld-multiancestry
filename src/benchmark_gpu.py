4063bfcf0b3d18634dc1675be2152449aacbc9f1
Charles Luo <k6luo@ucsd.edu>
2026-07-28 15:07:32 +0800

    GPU benchmark with decomposed stages

--- src/benchmark_gpu.py
+++ src/benchmark_gpu.py
@@ -1,9 +1,9 @@
-import time, numpy as np, pandas as pd, matplotlib.pyplot as plt, seaborn as sns
+import numpy as np, pandas as pd, matplotlib.pyplot as plt, seaborn as sns
 import torch
-from benchmark import time_one_run, generate_synthetic_G
-from ld_torch_gpu import ld_torch_gpu
+from benchmark import generate_synthetic_G
+from ld_torch_gpu import ld_torch_gpu, time_decomposed
 
-def benchmark_gpu(ld_func, window_sizes, n_samples, n_reps = 3):
+def decomposed_benchmark(window_sizes, n_samples, n_reps = 3):
 
     results = []
 
@@ -13,15 +13,19 @@ def benchmark_gpu(ld_func, window_sizes, n_samples, n_reps = 3):
         try:
             for j in range(n_reps):
 
+                total_t, sep = time_decomposed(G)
+
                 record = {
-                    "implementation" : ld_func.__name__,
                     "n_variants" : window_sizes[i],
-                    "elapsed_time" : time_one_run(ld_func, G)
+                    "transfer_in_time" : sep[0],
+                    "compute_time" : sep[1],
+                    "transfer_out_time" : sep[2],
+                    "total_time" : total_t
                 }
 
                 results.append(record)
 
-            print(f"Passed {ld_func.__name__} for window_size {window_sizes[i]}.")
+            print(f"Passed for window_size {window_sizes[i]}.")
 
         except torch.cuda.OutOfMemoryError:
             print(f"Caught CUDA OOM error at window size {window_sizes[i]}")
@@ -29,7 +33,7 @@ def benchmark_gpu(ld_func, window_sizes, n_samples, n_reps = 3):
             break
 
     df = pd.DataFrame(results)
-    df = df.groupby(["implementation","n_variants"])["elapsed_time"].min()
+    df = df.groupby(["n_variants"])[["transfer_in_time", "compute_time", "transfer_out_time","total_time"]].min()
 
     return df.reset_index()
 
@@ -40,19 +44,26 @@ if __name__ == "__main__":
 
     #Giving CUDA a warmp-up run before benchmarking
     small_G = np.random.randint(0,3,size = (50, 50))
-    ld_torch_gpu(small_G)
+    time_decomposed(small_G)
+
+    #Still using big-n to check for transfer times
 
     window_sizes = [5000, 8000, 10000, 15000, 20000, 30000, 40000]
     n_samples = 600
 
-    df_gpu = benchmark_gpu(ld_torch_gpu, window_sizes, n_samples)
+    df_gpu = decomposed_benchmark(window_sizes, n_samples)
 
-    sns.lineplot(data=df_gpu, x="n_variants", y="elapsed_time", hue="implementation", marker='o')
+    for col in ["transfer_in_time", "compute_time", "transfer_out_time"]:
+        plt.plot(df_gpu['n_variants'], df_gpu[col], label = col)
+
     plt.xscale('log')
     plt.yscale('log')
     plt.ylabel('runtime (seconds)')
-    plt.title("LD Large-n Runtime GPU (log-log)", fontdict={'size': 8})
+    plt.title("Decomposed GPU runtime (log-log)", fontdict={'size': 8})
+    plt.legend()
 
-    plt.savefig("figs/fig2_gpu_large_n.png", dpi=300, bbox_inches='tight')
+    plt.show()
 
-    df_gpu.to_csv("results/gpu_large_n.csv")
+    #plt.savefig("figs/fig2_gpu_large_n.png", dpi=300, bbox_inches='tight')
+
+    #df_gpu.to_csv("results/gpu_large_n.csv")
